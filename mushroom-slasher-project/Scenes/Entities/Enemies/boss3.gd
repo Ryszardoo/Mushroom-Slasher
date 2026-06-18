ab5a8a09c2174202a162b9755986408d9f7c5658
Olgierd Karpacz <156137609+OKarpacz@users.noreply.github.com>
2026-06-18 21:08:22 +0200

    Adding a Boss for LvL adding a bug fix and debug statment

--- mushroom-slasher-project/Scenes/Entities/Enemies/boss3.gd
+++ mushroom-slasher-project/Scenes/Entities/Enemies/boss3.gd
@@ -11,13 +11,13 @@ enum State{
 }
 
 @export_category("Stats")
-@export var speed: int = 120
-@export var attack_damage: int = 10
+@export var speed: int = 130
+@export var attack_damage: int = 30
 @export var attack_speed: float = 1.0
-@export var hitpoints: int = 100
+@export var hitpoints: int = 120
 @export var aggro_range: float = 256.0
 @export var attack_range: float = 80.0
-@export var exp_reward: int = 600
+@export var exp_reward: int = 800
 @export_category("Related Scenes")
 @export var death_packed: PackedScene
 
--- mushroom-slasher-project/Scenes/Main/game_scene.gd
+++ mushroom-slasher-project/Scenes/Main/game_scene.gd
@@ -6,13 +6,17 @@ func _ready() -> void:
 	var enemy_array: Array = get_tree().get_nodes_in_group("enemies")
 	for i in enemy_array:
 		i.died.connect(experience_gained)
-	var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		push_error("Didn't find player in group 'player' :()")
+		return
+
 	levelup.connect(player.calculate_stats)
 
 func experience_gained(exp_gain: int) -> void:
 	if PlayerData.level == LevelData.MAX_LEVEL:
 		return
-	var new_experience: int = PlayerData.experience * exp_gain
+	var new_experience: int = PlayerData.experience + exp_gain
 	if new_experience >= LevelData.LEVEL_THRESHOLDS[PlayerData.level - 1]:
 		level_up(new_experience)
 	else:
